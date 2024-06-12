06241ade8fcdb996fa973609d32afdb633af58de
Marien Giordani <giordanimarien@gmail.com>
2024-06-12 14:15:46 +0200

    Changed the way the slider is coded from vue template to full ipyvuetify to allow more control over it, and repaired the tooltip functionnality.

--- fast_pedago/gui/sliders/slider_input.py
+++ fast_pedago/gui/sliders/slider_input.py
@@ -1,58 +1,100 @@
 
 import ipyvuetify as v
-import traitlets
+import ipywidgets as widgets
 
 
-# TODO
-# Implement tooltip
-class SliderInput(v.VuetifyTemplate):
-    min = traitlets.Float(default_value=0).tag(sync=True)
-    max = traitlets.Float(default_value=100).tag(sync=True)
-    step = traitlets.Float(default_value=10).tag(sync=True)
-    label = traitlets.Unicode(default_value=None, allow_none=True).tag(sync=True)
-    tooltip = traitlets.Unicode(default_value=None, allow_none=True).tag(sync=True)
-    value = traitlets.Float(default_value=None, allow_none=True).tag(sync=True)
-    
-    @traitlets.default('template')
-    def _template(self):
-        return f'''
-        <template>
-            <v-row
-                justify="center"
-            >
-                <v-col
-                    class="ps-8 pe-0 py-1"
-                    cols=4
-                >
-                    <p>{self.label}</p>
-                </v-col>
-                
-                <v-col
-                    class="ps-3 py-1"
-                >
-                    <v-slider
-                        v-model="value"
-                        class="align-center pe-3"
-                        :max="max"
-                        :min="min"
-                        :step="step"
-                        hide-details
-                    >
-                        <template v-slot:append>
-                            <v-text-field
-                                v-model="value"
-                                class="mt-0 pt-0"
-                                variant="outlined"
-                                density="compact"
-                                hide-details
-                                single-line
-                                type="number"
-                                style="width: 60px"
-                            >
-                            </v-text-field>
-                        </template>
-                    </v-slider>
-                </v-col>
-            </v-row>
-        </template>
-        '''
+class SliderInput(v.Tooltip):
+    """
+    A slider input with a text field for more input possibilities.
+    The slider has an optional tooltip to display information.
+    """
+    def __init__(
+        self,
+        min: float=0,
+        max: float=100,
+        step: float=10,
+        label: str=None,
+        tooltip: str=None,
+        value: float=0,
+        **kwargs,
+        ):
+        """
+        :param min: min value of the slider
+        :param max: max value of the slider
+        :param step: step between two slider values 
+        :pram label: label to put before the slider
+        :param tooltip: tooltip to show when label, slider,
+            or text field is hovered
+        :param value: initial value of the slider
+        """
+        super().__init__(**kwargs)
+        
+        text_field = v.TextField(
+            v_model=value,
+            class_="mt-0 pt-0",
+            style_="width: 60px",
+            variant="outlined",
+            density="compact",
+            hide_details=True,
+            single_line=True,
+        )
+        
+        # slider is an instance variable to be able to get the value of
+        # its v_model from outside.
+        self.slider = v.Slider(
+            v_model=value,
+            max=max,
+            min=min,
+            step=step,
+            hide_details=True,
+            class_="align-center pe-3",
+        )
+
+        # Link the values of the slider with the associated text field.
+        # Since v_slots has an known issue in ipyvuetify, this is for 
+        # now the best way to do it.
+        widgets.jsdlink((self.slider,'v_model'),(text_field,'v_model'))
+        
+        self.v_slots=[{
+            'name': 'activator',
+            'variable': 'tooltip',
+            'children': v.Row(
+                v_bind="tooltip.attrs",
+                v_on="tooltip.on",
+                justify="center",
+                children=[
+                    v.Col(
+                        class_="ps-8 pe-0 py-1",
+                        cols=4,
+                        children=[
+                            v.Html(
+                                tag="p",
+                                children=[label],
+                            ),
+                        ],
+                    ),
+                    v.Col(
+                        class_="ps-3 py-1",
+                        children=[
+                            v.Row(
+                                children=[
+                                    v.Col(
+                                        class_="pa-0",
+                                        cols=9,
+                                        children=[self.slider],
+                                    ),
+                                    v.Col(
+                                        class_="pa-0",
+                                        children=[text_field],
+                                    ),
+                                ]
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        }]
+        
+        self.children=[
+            tooltip
+        ]
