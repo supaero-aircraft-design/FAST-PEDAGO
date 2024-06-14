a892eb1316525b49b658b8439db1326411e50a50
Marien Giordani <giordanimarien@gmail.com>
2024-06-14 10:19:55 +0200

    Added a container to chose which output graph to show and from which output file.

--- fast_pedago/gui/__init__.py
+++ fast_pedago/gui/__init__.py
@@ -2,3 +2,4 @@ from .header import Header
 from .footer import Footer
 from .inputs_container import InputsContainer
 from .graph_visualization_container import GraphVisualizationContainer
+from .outputs_selection_container import OutputsSelectionContainer
