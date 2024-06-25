4b0a72135158f045b8a4e6f4310e8981c0ad507c
Marien Giordani <giordanimarien@gmail.com>
2024-06-25 17:15:09 +0200

    Added a tutorial when the app opens, to explain how it works.

---
+++ fast_pedago/gui/resources/tutorial/text.py
@@ -0,0 +1,51 @@
+"""
+This file contains the text of the tutorial, sorted by slide.
+"""
+
+class Slide:
+    class Introduction:
+        WELCOME = "Welcome! In this app, you are going to "\
+            "design a derivative of a reference aircraft of your choice."
+
+        SELECTION = "When you have finished reading this tutorial, "\
+        "please select the source file of a reference aircraft below."
+        
+        NOTA = "(You can always come back to this tutorial and file "\
+        "selection by clicking on the logo of FAST-OAD at the top.)"
+    
+    class Process:
+        GLOBAL = "You will have the possibility to make Multi "\
+        "Disciplinary Analyses (MDA) or Multi Disciplinary Optimizations (MDO)."
+        
+        MDA = "The first one is a process that, with given inputs, will solve "\
+        "the multidisciplinary couplings using the different nested solvers in "\
+        "the model, and compute all outputs."
+        
+        MDO = "The second will, with a set of design variables and constraints, "\
+        "try to optimize an objective variable."
+
+    class Inputs:
+        EXPLANATIONS = "After choosing the source file, you will have to set your inputs:"
+        DASH_1 =  "- Choose between doing a MDA or a MDO"
+        DASH_2 = "- Write a name for the output"
+        DASH_3 = "- Use the sliders and buttons to configure your inputs"
+        DASH_4 = "- And launch your process !"
+    
+    class Launch:
+        EXPLANATIONS = "When launching, the inputs are retrieved and merged with the source "\
+        "file other inputs."\
+        "Then process is set up (association of all the modules together to make them loop) "\
+        "and the evolution of the process is plotted."
+
+    class Configuration:
+        EXPLANATIONS = "You have the possibility to see N2 and XDSM graphs. Both represents "\
+        "the way FAST-OAD modules interact with each other, what variables are inputs and outputs. "
+    
+    class Outputs:
+        SELECTION = "When you are done with your processes, you can open a second tab to "\
+        "display graphs on the outputs you got. "\
+        "Simply select all the outputs you want to compare, and they will display. "\
+        "For some graphs you can display variants:"
+        
+        WARNING = "WARNING : Some graphs can only display a single output. You will "\
+        "have to select which output you want to plot."
--- fast_pedago/gui/source_selection_container.py
+++ fast_pedago/gui/source_selection_container.py
@@ -6,10 +6,21 @@ import os.path as pth
 
 import ipyvuetify as v
 
+from .resources import Slide
+from fast_pedago import gui
 from fast_pedago import source_data_files
 from fast_pedago.utils import (
-    _list_available_reference_file
+    _list_available_reference_file,
+    _image_from_path,
 )
+
+
+FAST_OAD_LOGO = "logo_fast_oad_main_menu.jpg"
+
+INPUTS_GIF = "inputs.gif"
+LAUNCH_GIF = "launch.gif"
+N2_GIF = "n2.gif"
+OUTPUTS_GIF = "outputs.gif"
 
 
 class SourceSelectionContainer(v.Col):
@@ -20,9 +31,9 @@ class SourceSelectionContainer(v.Col):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
+        self._load_images()
         self._build_layout()
     
-
 
     def _build_layout(self):
         self.class_ = "pa-8"
@@ -39,16 +50,151 @@ class SourceSelectionContainer(v.Col):
             items=reference_file_list,
         )
         
+        tutorial_carousel = v.Carousel(
+            height="58vh",
+            width="70vw",
+            hide_delimiters=True,
+            children=[
+                v.CarouselItem(
+                    children=[
+                        v.Row(
+                            justify="center",
+                            align="center",
+                            children=[
+                                v.Html(
+                                    tag="div",
+                                    children=[Slide.Introduction.WELCOME],
+                                ),
+                            ],
+                        ),
+                        v.Row(
+                            justify="center",
+                            align="center",
+                            children=[self.fast_oad_logo],
+                        ),
+                        v.Row(
+                            class_="px-12",
+                            justify="center",
+                            align="center",
+                            children=[
+                                v.Html(
+                                    tag="div",
+                                    children=[Slide.Introduction.SELECTION],
+                                ),
+                            ],
+                        ),
+                        v.Row(
+                            class_="px-12",
+                            justify="center",
+                            align="center",
+                            children=[
+                                v.Html(
+                                    tag="div",
+                                    children=[Slide.Introduction.NOTA],
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+                v.CarouselItem(
+                    children=[
+                        v.Row(
+                            justify="center",
+                            align="top",
+                            children=[
+                                v.Col(cols=2, lg=3),
+                                v.Col(
+                                    cols=4,
+                                    lg=3,
+                                    children=[
+                                        v.Html(
+                                            class_="mb-6",
+                                            style_="font-size: 25px;",
+                                            tag="div",
+                                            children=[Slide.Inputs.EXPLANATIONS],
+                                        ),
+                                        v.Html(tag="div", children=[Slide.Inputs.DASH_1]),
+                                        v.Html(tag="div", children=[Slide.Inputs.DASH_2]),
+                                        v.Html(tag="div", children=[Slide.Inputs.DASH_3]),
+                                        v.Html(tag="div", children=[Slide.Inputs.DASH_4]),
+                                    ],
+                                ),
+                                v.Col(
+                                    cols=5,
+                                    lg=4,
+                                    children=[self.inputs_gif],
+                                ),
+                                v.Col(),
+                            ],
+                        ),
+                    ],
+                ),
+                v.CarouselItem(
+                    children=[
+                        v.Row(
+                            class_="pb-5 px-12 mx-12",
+                            justify="center",
+                            align="center",
+                            children=[Slide.Launch.EXPLANATIONS]
+                        ),
+                        v.Row(
+                            justify="center",
+                            align="center",
+                            children=[self.launch_gif],
+                        ),
+                    ],
+                ),
+                v.CarouselItem(
+                    children=[
+                        v.Row(
+                            class_="pb-5 px-12 mx-12",
+                            justify="center",
+                            align="center",
+                            children=[Slide.Configuration.EXPLANATIONS]
+                        ),
+                        v.Row(
+                            justify="center",
+                            align="center",
+                            children=[self.n2_gif],
+                        ),
+                    ],
+                ),
+                v.CarouselItem(
+                    children=[
+                        v.Row(
+                            class_="pb-5 px-12 mx-12",
+                            justify="center",
+                            align="center",
+                            children=[Slide.Outputs.SELECTION]
+                        ),
+                        v.Row(
+                            justify="center",
+                            align="center",
+                            children=[self.outputs_gif],
+                        ),
+                        v.Row(
+                            class_="pt-2 px-12 mx-12",
+                            justify="center",
+                            align="center",
+                            children=[
+                                v.Html(
+                                    style_="font-size: 14px; color: red;",
+                                    tag="div",
+                                    children=[Slide.Outputs.WARNING]
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+        
         # TODO: Write tutorial/explanations on fast-oad
         self.children=[
             v.Row(
                 justify="center",
-                children=["Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-                          "Sed non risus. Suspendisse lectus tortor, dignissim sit amet, "
-                          "adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. "
-                          "Maecenas ligula massa, varius a, semper congue, euismod non, mi. "
-                          "Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, "
-                          "non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, "
+                children=[
+                    tutorial_carousel,
                 ],
             ),
             v.Row(
@@ -59,3 +205,21 @@ class SourceSelectionContainer(v.Col):
                 ],
             ),
         ]
+
+
+    def _load_images(self):
+        """
+        Loads tutorial images and gifs as instance variables to call them during
+        the layout building.
+        """
+        resources_path = pth.join(pth.dirname(gui.__file__), "resources")
+        tutorial_resources_path = pth.join(resources_path, "tutorial")
+
+        self.fast_oad_logo = _image_from_path(pth.join(resources_path, FAST_OAD_LOGO), max_height="40vh")
+        
+        self.inputs_gif = _image_from_path(pth.join(tutorial_resources_path, INPUTS_GIF), max_height="70vh")
+        self.launch_gif = _image_from_path(pth.join(tutorial_resources_path, LAUNCH_GIF), max_height="50vh")
+        self.n2_gif = _image_from_path(pth.join(tutorial_resources_path, N2_GIF), max_height="50vh")
+        self.outputs_gif = _image_from_path(pth.join(tutorial_resources_path, OUTPUTS_GIF), max_height="35vh")
+
+        
